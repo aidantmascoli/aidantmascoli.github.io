f909ca1a238852ef2ace9d3faa01e6e984189249
denayeisnotokay <aidantm717@gmail.com>
2025-07-13 21:25:58 -0400

    finish resume page

--- src/app/resume/page.js
+++ src/app/resume/page.js
@@ -109,7 +109,7 @@ export default function Resume() {
                         <div className="flex flex-col gap-2">
                             <span className="flex justify-between items-center text-yellow-600">
                                 <h4 className="">Kulite Semiconductor Products</h4>
-                                <h5>May&ndash;July 2022</h5>
+                                <h5>May&ndash;July 2024</h5>
                             </span>
                             <ul className="list-disc pl-6">
                                 <li>Accounted the company’s greenhouse gas emissions over 1 year by collecting and
@@ -137,86 +137,67 @@ export default function Resume() {
                     className="mb-16 p-8 bg-green-50"
                 >
                     <h2 className="text-green-600 mb-4">Awards & Honors</h2>
-                    USA Computing Olympiad (USACO):
-                    Silver Qualifier (Bronze Division perfect score)	2022
-                    Cornell University Programming Contest:
-                    Third Place Team Nationally	2023
-                    Fourth Place Team Nationally	2022
-                    CyberStart America:
-                    Advanced to Bronze Level	2022
-                    Top 30 Highest Scores in New Jersey	2022
-                    National Cyber Scholarship Candidate	2022
-                    Scholastic Art and Writing Awards:
-                    Science Fiction / Fantasy - Honorable Mention	2022-2023
-                    Critical Essay - Silver Key 	2020-2021
-                    Short Story - Gold Key	2020-2021
-                    Flash Fiction - Silver Key	2020-2021
-
                     <div className="space-y-4">
                         <div className="flex flex-col gap-1">
-                            <h4 className="text-green-700">American Mathematics Competitions (AMC)</h4>
+                            <h5 className="text-green-700">American Mathematics Competitions (AMC)</h5>
                             <span className="pl-6 flex flex-row justify-between">
-                                <h6>Qualified for AIME</h6>
-                                <p>2020-2022</p>
+                                <p>Qualified for AIME</p>
+                                <p>2020&ndash;2022</p>
                             </span>
                             <span className="pl-6 flex flex-row justify-between">
-                                <h6>Scored 6 out of 15 on the AIME</h6>
-                                <p>2020-2021</p>
+                                <p>Scored 6 out of 15 on the AIME</p>
+                                <p>2020&ndash;2021</p>
                             </span>
                         </div>
                         <div className="flex flex-col gap-1">
-                            <h4 className="text-green-700">American Computer Science League (ACSL)</h4>
+                            <h5 className="text-green-700">American Computer Science League (ACSL)</h5>
                             <span className="pl-6 flex flex-row justify-between">
-                                <h6>Bronze Award in National Senior Division Finals</h6>
+                                <p>Bronze Award in National Senior Division Finals</p>
                                 <p>2022</p>
                             </span>
                             <span className="pl-6 flex flex-row justify-between">
-                                <h6>4th Nationally in Intermediate Division Finals</h6>
+                                <p>4th Nationally in Intermediate Division Finals</p>
                                 <p>2021</p>
                             </span>
                         </div>
                         <div className="flex flex-col gap-1">
-                            <h4 className="text-green-700">American Mathematics Competitions (AMC)</h4>
+                            <h5 className="text-green-700">USA Computing Olympiad (USACO)</h5>
                             <span className="pl-6 flex flex-row justify-between">
-                                <h6>Qualified for AIME</h6>
-                                <p>2020-2022</p>
-                            </span>
-                            <span className="pl-6 flex flex-row justify-between">
-                                <h6>Scored 6 out of 15 on the AIME</h6>
-                                <p>2020-2021</p>
+                                <p>Silver Qualifier (Bronze Division perfect score)</p>
+                                <p>2022</p>
                             </span>
                         </div>
                         <div className="flex flex-col gap-1">
-                            <h4 className="text-green-700">American Mathematics Competitions (AMC)</h4>
+                            <h5 className="text-green-700">Cornell University Programming Contest</h5>
                             <span className="pl-6 flex flex-row justify-between">
-                                <h6>Qualified for AIME</h6>
-                                <p>2020-2022</p>
+                                <p>Third Place Team Nationally</p>
+                                <p>2023</p>
                             </span>
                             <span className="pl-6 flex flex-row justify-between">
-                                <h6>Scored 6 out of 15 on the AIME</h6>
-                                <p>2020-2021</p>
-                            </span>
-                        </div>
-                        <div className="flex flex-col gap-1">
-                            <h4 className="text-green-700">American Mathematics Competitions (AMC)</h4>
-                            <span className="pl-6 flex flex-row justify-between">
-                                <h6>Qualified for AIME</h6>
-                                <p>2020-2022</p>
-                            </span>
-                            <span className="pl-6 flex flex-row justify-between">
-                                <h6>Scored 6 out of 15 on the AIME</h6>
-                                <p>2020-2021</p>
+                                <p>Fourth Place Team Nationally</p>
+                                <p>2022</p>
                             </span>
                         </div>
                         <div className="flex flex-col gap-1">
-                            <h4 className="text-green-700">American Mathematics Competitions (AMC)</h4>
+                            <h5 className="text-green-700">CyberStart America</h5>
                             <span className="pl-6 flex flex-row justify-between">
-                                <h6>Qualified for AIME</h6>
-                                <p>2020-2022</p>
+                                <p>Advanced to Bronze Level</p>
+                                <p>2022</p>
                             </span>
                             <span className="pl-6 flex flex-row justify-between">
-                                <h6>Scored 6 out of 15 on the AIME</h6>
-                                <p>2020-2021</p>
+                                <p>National Cyber Scholarship Candidate</p>
+                                <p>2022</p>
+                            </span>
+                        </div>
+                        <div className="flex flex-col gap-1">
+                            <h5 className="text-green-700">Scholastic Art and Writing Awards</h5>
+                            <span className="pl-6 flex flex-row justify-between">
+                                <p>Science Fiction / Fantasy - Honorable Mention</p>
+                                <p>2022&ndash;2023</p>
+                            </span>
+                            <span className="pl-6 flex flex-row justify-between">
+                                <p>Short Story - Gold Key</p>
+                                <p>2020&ndash;2021</p>
                             </span>
                         </div>
                     </div>
@@ -225,27 +206,45 @@ export default function Resume() {
                     ref={el => sections.current[3] = el}
                     className="mb-16 p-8 bg-blue-50"
                 >
-                    <h2 className="text-blue-600 mb-4">Leadership</h2>
-                    <p className="text-default-600 mb-4">In my free time, I like to either work on personal coding
-                        projects, play with breadboard electronics, create and edit gaming content, play guitar, or create
-                        beats, remix songs, or just play around with samples on Ableton Live. When I can I love going to
-                        concerts, and travelling.</p>
-                    <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-                        <div className="aspect-video relative">
-                            <Image
-                                src="/images/circuit.jpg"
-                                alt="Breadboard Circuit"
-                                fill
-                                className="object-cover"
-                            />
+                    <h2 className="text-blue-500 mb-4">Leadership</h2>
+                    <div className="space-y-4">
+                        <div className="flex flex-col gap-2">
+                            <h4 className="text-blue-600">Student Government</h4>
+                            <div className="pl-6">
+                                <span className="flex justify-between items-center text-blue-700">
+                                    <h5>Senior Leadership Committee</h5>
+                                    <p>2022&ndash;2023</p>
+                                </span>
+                                <ul className="list-disc pl-6">
+                                    <li>Responsible for planning and executing school events, including social events and fundraisers.</li>
+                                    <li>In charge of budgeting and management of student event funds.</li>
+                                </ul>
+                            </div>
                         </div>
-                        <div className="aspect-video relative">
-                            <Image
-                                src="/images/ableton.jpg"
-                                alt="Ableton Live"
-                                fill
-                                className="object-cover"
-                            />
+                        <div className="flex flex-col gap-2">
+                            <h4 className="text-blue-600">DramaTech Theatre</h4>
+                            <div className="pl-6">
+                                <span className="flex justify-between items-center text-blue-700">
+                                    <h5>Executive Council - Secretary</h5>
+                                    <p>April 2025&ndash;Present</p>
+                                </span>
+                                <ul className="list-disc pl-6">
+                                    <li>Elected to club leadership position for the 2025-2026 academic year.</li>
+                                    <li>Responsibilities include attending leadership meetings and providing reports,
+                                    taking meeting minutes, running club elections, and managing the organization’s
+                                    slack workspace and website, and replying to or forwarding emails sent to the club.</li>
+                                </ul>
+                                <span className="flex justify-between items-center text-blue-700">
+                                    <h5>Technical Services Council - Member</h5>
+                                    <p>January 2025&ndash;Present</p>
+                                </span>
+                                <ul className="list-disc pl-6">
+                                    <li>Initially appointed as an intern, promoted to a full lifetime member in April 2025.</li>
+                                    <li>Responsible for general maintenance and upkeep, as well as general non-production
+                                    improvements such as purchasing and installing new equipment, and general safety and
+                                    emergency response procedures</li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -253,27 +252,31 @@ export default function Resume() {
                     ref={el => sections.current[4] = el}
                     className="mb-16 p-8 bg-violet-50"
                 >
-                    <h2 className="text-violet-600 mb-4">Skills & Expertise</h2>
-                    <p className="text-default-600 mb-4">In my free time, I like to either work on personal coding
-                        projects, play with breadboard electronics, create and edit gaming content, play guitar, or create
-                        beats, remix songs, or just play around with samples on Ableton Live. When I can I love going to
-                        concerts, and travelling.</p>
-                    <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-                        <div className="aspect-video relative">
-                            <Image
-                                src="/images/circuit.jpg"
-                                alt="Breadboard Circuit"
-                                fill
-                                className="object-cover"
-                            />
+                    <h2 className="text-violet-500 mb-4">Skills & Expertise</h2>
+                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 py-4 gap-y-2 gap-x-6">
+                        <div>
+                            <h5 className="text-violet-600">Programming</h5>
+                            <p>Java, Python, C, C#, C++, HTML, CSS, JavaScript, PHP, SQL, React, Node.js, Django, Unity, DirectX</p>
                         </div>
-                        <div className="aspect-video relative">
-                            <Image
-                                src="/images/ableton.jpg"
-                                alt="Ableton Live"
-                                fill
-                                className="object-cover"
-                            />
+                        <div>
+                            <h5 className="text-violet-600">Platforms</h5>
+                            <p>Amazon AWS, Google Cloud Platform, Firebase</p>
+                        </div>
+                        <div>
+                            <h5 className="text-violet-600">Hardware</h5>
+                            <p>Arduino, ARM mbed microcontroller, FPGAs, oscilloscope</p>
+                        </div>
+                        <div>
+                            <h5 className="text-violet-600">Software</h5>
+                            <p>Altera Quartus II, NI LabVIEW, GitHub, Adobe Creative Cloud (Photoshop, Illustrator, Premiere Pro, After Effects), Ableton Live, Logic Pro X, Microsoft Office (Word, Excel, PowerPoint, Access)</p>
+                        </div>
+                        <div>
+                            <h5 className="text-violet-600">Communication</h5>
+                            <p>Leadership reports, production reports, writing research papers/reports, presentations, performance</p>
+                        </div>
+                        <div>
+                            <h5 className="text-violet-600">Languages</h5>
+                            <p>English (first), Chinese (beginner)</p>
                         </div>
                     </div>
                 </div>
